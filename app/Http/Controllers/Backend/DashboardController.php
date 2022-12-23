fcc529ca522abfcdd153fe65e3d074390b92a074
masudKhanGit <masud.git.developer@gmail.com>
2022-12-23 17:46:21 +0600

    DashboardController

--- app/Http/Controllers/Backend/DashboardController.php
+++ app/Http/Controllers/Backend/DashboardController.php
@@ -3,10 +3,20 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    public function index() {
-        return view('backend.modules.index');
+    public function index()
+    {
+        $post_count = Post::where('user_id', Auth::user()->id)
+            ->get()
+            ->count();
+        $active_post = Post::where('user_id', Auth::user()->id)
+            ->where('status', 'ACTIVE')
+            ->get()
+            ->count();
+        return view('backend.dashboard.index', compact('post_count', 'active_post'));
     }
 }
--- resources/views/backend/dashboard/index.blade.php
+++ resources/views/backend/dashboard/index.blade.php
@@ -5,7 +5,11 @@
     <div class="row">
         <div class="col-xl-3 col-md-6">
             <div class="card bg-primary text-white mb-4">
-                <div class="card-body">Primary Card</div>
+                <div class="card-body"></div>
+                <div class="p-3">
+                    <h3>Total Post: {{ $post_count }}</h3>
+                    <h3>Total Active Post: {{ $active_post }}</h3>
+                </div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
                     <a class="small text-white stretched-link" href="#">View Details</a>
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
